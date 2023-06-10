c8bd120412d8fb173f6752cb16030296ef04e639
longwatermelon <longwatermelon1@gmail.com>
2023-06-10 15:36:25 -0700

    more effects

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 mod video;
 
+use rand::Rng;
+
+fn t(minute: i32, seconds: i32) -> f32 {
+    minute as f32 + seconds as f32 / 60.
+}
+
 fn main() {
-    video::create("res/bateman.mp4", "no-audio.mp4", &(0..18).map(|x| x as f32 * 0.67).collect::<Vec<f32>>()).expect("Error in creating video.");
+    let songs: Vec<(&str, Vec<f32>)> = vec![
+        ("res/metamorphosis.mp3", (0..18).map(|x| x as f32 * 0.67).collect::<Vec<f32>>()),
+        ("res/neon-blade.mp4", _)
+    ];
+    let song: (&str, Vec<f32>) = songs[rand::thread_rng().gen_range(0..songs.len())].clone();
+
+    video::create("res/bateman.mp4", "no-audio.mp4", &song.1,
+            &[t(0, 2), t(0, 7), t(0, 11), t(0, 16), t(0, 22), t(0, 24), t(0, 27), t(0, 30), t(0, 34),
+              t(1, 6), t(1, 10), t(1, 14), t(1, 17), t(1, 22), t(1, 25), t(1, 29), t(1, 39), t(1, 40), t(1, 43), t(1, 49), t(1, 56), t(1, 58),
+              t(2, 6), t(2, 9), t(2, 14), t(2, 16), t(2, 19), t(2, 30), t(2, 32), t(2, 34), t(2, 40), t(2, 42), t(2, 46), t(2, 52), t(2, 54), t(2, 55),
+              t(3, 6), t(3, 11), t(3, 13), t(3, 15), t(3, 17), t(3, 19), t(3, 22), t(3, 29), t(3, 36), t(3, 42), t(3, 55), t(3, 59),
+              t(4, 2), t(4, 5), t(4, 11), t(4, 12), t(4, 16), t(4, 20), t(4, 24), t(4, 29), t(4, 34), t(4, 37), t(4, 42), t(4, 46), t(4, 47), t(4, 48), t(4, 52), t(4, 53), t(4, 55), t(4, 56), t(4, 58), t(4, 59),
+              t(5, 0), t(5, 1), t(5, 2), t(5, 4), t(5, 8), t(5, 10), t(5, 11), t(5, 14), t(5, 17), t(5, 18), t(5, 21), t(5, 26), t(5, 29), t(5, 34), t(5, 41), t(5, 46), t(5, 51), t(5, 56),
+              t(6, 0), t(6, 6), t(6, 10), t(6, 19), t(6, 57),
+              t(8, 15), t(8, 20), t(8, 32), t(8, 37), t(8, 50), t(8, 52),
+              t(9, 0), t(9, 14)]
+    ).expect("Error in creating video.");
+
     std::process::Command::new("rm").arg("output.mp4").output().unwrap();
     println!("Adding audio...");
-    std::process::Command::new("ffmpeg").arg("-i").arg("no-audio.mp4").arg("-i").arg("res/metamorphosis.mp3").arg("-c:v").arg("copy").arg("-c:a").arg("aac").arg("-strict").arg("experimental")
+    std::process::Command::new("ffmpeg").arg("-i").arg("no-audio.mp4").arg("-i").arg(song.0).arg("-c:v").arg("copy").arg("-c:a").arg("aac").arg("-strict").arg("experimental")
         .arg("-shortest").arg("output.mp4").output().expect("Failed to overlay audio.");
 
     println!("Cleaning up...");
--- src/video.rs
+++ src/video.rs
@@ -1,8 +1,10 @@
 use opencv::{prelude::*, core, videoio, Result};
 use opencv::videoio::{VideoCapture, VideoWriter};
+use opencv::core::{Scalar, MatTrait};
+use opencv::imgproc::{cvt_color, COLOR_BGR2HSV, COLOR_HSV2BGR, warp_affine};
 use rand::Rng;
 
-pub fn create(input: &str, output: &str, beats: &[f32]) -> Result<()> {
+pub fn create(input: &str, output: &str, beats: &[f32], cuts: &[f32]) -> Result<()> {
     let mut video: VideoCapture = VideoCapture::from_file(input, videoio::CAP_ANY)?; // 0 is the default camera
     let w: i32 = video.get(videoio::CAP_PROP_FRAME_WIDTH)? as i32;
     let h: i32 = video.get(videoio::CAP_PROP_FRAME_HEIGHT)? as i32;
@@ -13,24 +15,88 @@ pub fn create(input: &str, output: &str, beats: &[f32]) -> Result<()> {
 
     for i in 1..beats.len() {
         println!("Writing beat interval {:.2} to {:.2}...", beats[i - 1], beats[i]);
-        write_beat_interval(&mut out, &mut video, beats[i] - beats[i - 1])?;
+        write_beat_interval(&mut out, &mut video, beats[i] - beats[i - 1], cuts)?;
     }
 
     out.release()?;
     Ok(())
 }
 
-fn write_beat_interval(writer: &mut VideoWriter, video: &mut VideoCapture, beat_len: f32) -> Result<()> {
+fn saturate(frame: &Mat, saturation_factor: f32) -> Mat {
+    let mut hsv_image = Mat::default();
+    cvt_color(frame, &mut hsv_image, COLOR_BGR2HSV, 0).unwrap();
+
+    for y in 0..hsv_image.rows() {
+        for x in 0..hsv_image.cols() {
+            let pixel = hsv_image.at_2d_mut::<opencv::core::Vec3b>(y, x).unwrap();
+            let saturation = pixel[1] as i16;
+            let adjusted_saturation = (saturation as f32 * saturation_factor) as i16;
+            pixel[1] = adjusted_saturation.max(0).min(255) as u8;
+        }
+    }
+
+    let mut output_image = Mat::default();
+    cvt_color(&hsv_image, &mut output_image, COLOR_HSV2BGR, 0).unwrap();
+    output_image
+}
+
+fn shift(frame: &Mat, xshift: i32, yshift: i32) -> Mat {
+    let shift_matrix = Mat::from_slice_2d(&[
+        &[1.0, 0.0, xshift as f64],
+        &[0.0, 1.0, yshift as f64]
+    ]).unwrap();
+
+    let mut shifted_image = frame.clone();
+    warp_affine(
+        frame,
+        &mut shifted_image,
+        &shift_matrix,
+        frame.size().unwrap(),
+        opencv::imgproc::INTER_LINEAR,
+        opencv::core::BORDER_CONSTANT,
+        Scalar::new(0., 0., 0., 0.),
+    ).unwrap();
+
+    shifted_image
+}
+
+fn write_beat_interval(writer: &mut VideoWriter, video: &mut VideoCapture, beat_len: f32, cuts: &[f32]) -> Result<()> {
     let frames: i32 = (30. * beat_len) as i32;
     let total_frames: i32 = video.get(videoio::CAP_PROP_FRAME_COUNT)? as i32;
-    let begin: i32 = rand::thread_rng().gen_range(0..(total_frames - frames));
+
+    let mut begin: f32;
+    loop {
+        begin = rand::thread_rng().gen_range(0..(total_frames - frames)) as f32;
+
+        let mut valid: bool = true;
+        for &cut in cuts {
+            if begin < cut && cut - begin < frames as f32 {
+                valid = false;
+                break;
+            }
+        }
+
+        if valid {
+            break;
+        }
+    }
+
+    let mut saturation: f32 = 3.;
 
     video.set(videoio::CAP_PROP_POS_FRAMES, begin as f64)?;
-    for _ in 0..frames {
+    for i in 0..frames {
+        let progress: f32 = i as f32 / frames as f32;
+
         let mut frame: Mat = Mat::default();
         video.read(&mut frame)?;
+        let adjusted: Mat = shift(&saturate(&frame, saturation),
+            (25. * f32::exp(-10. * progress) * f32::cos(1.5 * i as f32 + 0.5)) as i32,
+            (25. * f32::exp(-10. * progress) * f32::sin(2. * i as f32)) as i32
+        );
 
-        writer.write(&frame)?;
+        saturation = 1. + 2. * f32::exp(-(2. * i as f32) / (frames as f32));
+
+        writer.write(&adjusted)?;
     }
 
     Ok(())
