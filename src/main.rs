a4fa0ee7feadf3e7beb26dc45e3869dcfb7e5e06
longwatermelon <longwatermelon1@gmail.com>
2023-06-29 10:58:00 -0700

    description field

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod video;
 use rand::Rng;
 use std::fs;
 use std::process::{exit, Command};
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = std::env::args().skip(1).collect();
@@ -44,12 +45,33 @@ fn main() {
             }
         }
 
-        let audios: Vec<String> = (0..2).map(|_| {
+        let audios: Vec<String> = (0..6).map(|_| {
             let index: usize = rand::thread_rng().gen_range(0..paths.len());
             paths.remove(index)
         }).collect();
 
+        let mut audio_info: HashMap<&str, (&str, &str)> = HashMap::new();
+        audio_info.insert("res/compilation/callme.mp3", ("Call Me by plenka", "https://youtu.be/1aAY7EI3u8c"));
+        audio_info.insert("res/compilation/live-another-day.mp3", ("Live Another Day by Kordhell", "https://youtu.be/7pmUQJ5KAlU"));
+        audio_info.insert("res/compilation/metamorphosis.mp3", ("METAMORPHOSIS by INTERWORLD", "https://youtu.be/lJvRohYSrZM"));
+        audio_info.insert("res/compilation/override.mp3", ("Override by KSLV", "https://youtu.be/NS8DPG62Fto"));
+        audio_info.insert("res/compilation/rave.mp3", ("Rave by Dxrk ダーク", "https://youtu.be/PTZgxW_3LIQ"));
+        audio_info.insert("res/compilation/sahara.mp3", ("Sahara by Hensonn", "https://youtu.be/pIZ0QRWK0zg"));
+
+        let mut desc: String = String::new();
+        let mut timestamp: u64 = 0;
+        for audio in &audios {
+            desc.push_str(format!("{}:{:0>2} {} ({})\n", timestamp / 60, timestamp % 60,
+                    audio_info.get(audio.as_str()).unwrap().0,
+                    audio_info.get(audio.as_str()).unwrap().1
+                ).as_str()
+            );
+            let seconds: u64 = mp3_duration::from_path(audio.as_str()).unwrap().as_secs();
+            timestamp += seconds;
+        }
+
         println!("Audios: {:?}", audios);
+        println!("Description:\n============\nTIMESTAMPS\n{}============", desc);
 
         let mut ffmpeg_cmd: String = String::from("ffmpeg");
         for audio in &audios {
@@ -63,7 +85,7 @@ fn main() {
         Command::new("sh").args(["-c", ffmpeg_cmd.as_str()]).output().unwrap();
 
         println!("Overlay image...");
-        ffmpeg_cmd = String::from("ffmpeg -loop 1 -i res/compilation/backgrounds/0.png -i audio.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest static.mp4");
+        ffmpeg_cmd = String::from("ffmpeg -loop 1 -i res/compilation/backgrounds/0.png -i audio.mp3 -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest output/0.mp4");
         Command::new("sh").args(["-c", ffmpeg_cmd.as_str()]).output().unwrap();
 
         println!("Done");
